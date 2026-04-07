dede8fd655121a020cb646aea3937f49b035a89b
Neil Dime <dime.neil03@gmail.com>
2026-04-07 21:32:54 +0800

    Added Cors Origins

--- main.py
+++ main.py
@@ -13,7 +13,10 @@ app = FastAPI(title="OSA Service Portal API")
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:3000"],
+    allow_origins=[
+        "https://osa-service-portal-fastapi.vercel.app",
+        "https://osaserviceportal.vercel.app",
+    ],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
