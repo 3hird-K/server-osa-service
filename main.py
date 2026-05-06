99a9e8f5b04f13db777e1a72293a1099aa55cf20
Neil Dime <dime.neil03@gmail.com>
2026-05-06 17:25:47 +0800

    added: local dev

--- main.py
+++ main.py
@@ -25,7 +25,9 @@ app.add_middleware(
     allow_origins=[
         "https://osa-service-portal-fastapi.vercel.app",
         "https://osaserviceportal.vercel.app",
-        "http://localhost:3000"
+        "http://localhost:3000",
+        "http://localhost:8081",
+        "http://localhost:8082"
     ],
     allow_credentials=True,
     allow_methods=["*"],
